aacaaaf62858ca58fc8d237fe67ef59217100073
Tulio Troncoso <tuliot@live.com>
2025-02-27 16:16:06 -0500

    Fix loading environment variables in launchagent

--- device_switch_server.py
+++ device_switch_server.py
@@ -4,14 +4,15 @@ import os
 
 KEYBOARD_MAC = os.getenv("KEYBOARD_MAC")
 TRACKPAD_MAC = os.getenv("TRACKPAD_MAC")
-PORT = os.getenv("PORT")
+BLUEUTIL_PATH = os.getenv("BLUEUTIL_PATH")
+PORT = int(os.getenv("PORT"))
 
 class RequestHandler(BaseHTTPRequestHandler):
   def do_GET(self):
       if self.path == "/unpair":
           self.wfile.write(b"Unpairing keyboard and trackpad...\n")
-          subprocess.run(["blueutil", "--unpair", KEYBOARD_MAC])
-          subprocess.run(["blueutil", "--unpair", TRACKPAD_MAC])
+          subprocess.run([BLUEUTIL_PATH, "--unpair", KEYBOARD_MAC])
+          subprocess.run([BLUEUTIL_PATH, "--unpair", TRACKPAD_MAC])
           self.send_response(200, "Unpairing devices")
           self.send_header("Content-type", "text/plain")
           self.end_headers()
@@ -21,6 +22,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=PORT):
     server_address = ("0.0.0.0", port)
+    print(f"Starting server on {server_address}")
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
